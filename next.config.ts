76cd4dbbcdc46586a4566e02d7704509ab51d644
linhhuynhcoding <nhatlinh.6449@gmail.com>
2025-05-13 21:28:42 +0700

    chore: config i18n

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  i18n: {
+    locales: ['en', 'vi'],
+    defaultLocale: 'vi',
+    localeDetection: false,
+
+  },
   images: {
     remotePatterns: [
       {
---
+++ src/lib/slugMap.ts
@@ -0,0 +1,10 @@
+export const slugMap = {
+  court: {
+    en: 'court',
+    vi: 'san-bong',
+  },
+  product: {
+    en: 'product',
+    vi: 'san-pham-the-thao',
+  },
+}
